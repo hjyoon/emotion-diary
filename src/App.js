79d6821cd3005d004f5ecbfa943612a64f38af5f
hjyoon <hjyoon@kakao.com>
2022-04-17 06:24:17 +0900

    Use BrowserRouter for routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">App.js</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/diary" element={<Diary />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
